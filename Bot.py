8bcac0f65f7b6d07fb3a64b7bd6b04eaba6c3e75
Phuongaz <42109045+ZzKino@users.noreply.github.com>
2021-07-26 20:16:57 +0700

    lo

--- Bot.py
+++ Bot.py
@@ -1,21 +1,46 @@
-from discord.ext import commands
 import os
 import sys
 import yaml
+import requests
+import discord
 
-bot = commands.Bot(command_prefix='>')
+class BotClient(discord.Client):
 
-@bot.command()
-async def ping(ctx):
-	await ctx.send('pong')
+	async def on_ready(self):
+		print('Logged on as', self.user)
 
-with open(os.path.dirname(os.path.abspath(__file__)) + "/config.yaml", 'r') as stream:
-	try:
-		yml = yaml.safe_load(stream)
-		if yml['token'] == '':
-			print('Token missing')
-			sys.exit()
-	except yaml.YAMLError as exc:
-		print(exc)
+	async def on_message(self, message):
+		cmd = message.content
+		if message.author == self.user:
+			return
+		if cmd == ".ping":
+			await channel.send("pong")
+		if cmd == ".players":
+			response = requests.get("https://api.mcsrvstat.us/bedrock/2/mc.locm.cf:19132")
+			if (response.json()['online']):
+				players = response.json()['players']
+				await channel.send("LOCM Players: " + str(players["online"]) + "/" + str(players["max"]))
+				return
+			await channel.send("Server Offline")
+class config:
 
-bot.run(yml['token'])
+	def __init__(self):
+		with open(os.path.dirname(os.path.abspath(__file__)) + "/config.yaml", 'r') as stream:
+			try:
+				yml = yaml.safe_load(stream)
+				if yml["token"] == '':
+					print("Token missing")
+					sys.exit()
+			except yaml.YAMLError as exc:
+				print(exc)
+		self.config = yml
+
+	def get_token(self):
+		return self.config["token"]
+
+	def get_admins(self):
+		return self.config["admins"]
+
+client = BotClient()
+config = config()
+client.run(config.get_token())
